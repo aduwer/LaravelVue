1abf9cff06ca8e0f8d7f66d87a7f63417bdf84e7
aduwer <adrian.duwer@studiosoftware.com>
2022-07-07 09:33:29 +0200

    UserRegisterRequest

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class UserRegisterRequest extends FormRequest
+{
+
+    /**
+     * @param Request $request
+     * @return \string[][]
+     */
+    public function rules(Request $request): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8'],
+            'name' => ['required'],
+            'surname' => ['required']
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'email' => 'Email',
+            'password' => 'Hasło',
+            'name' => 'Imię',
+            'surname' => 'Nazwisko'
+        ];
+    }
+}
